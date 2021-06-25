5452b75cf05b9721d0da5abea47964f21b36db76
Gabriel Avila <gabriel.avila.ga09@gmail.com>
2021-06-25 14:02:09 -0600

    first commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,84 @@
+package com.skilldistillery.filmquery.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
+	private String user = "student";
+	private String pass = "student";
+	private String sql;
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Film findFilmById(int filmId) throws SQLException {
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		sql = "SELECT * FROM film WHERE id = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmId);
+		ResultSet rs = stmt.executeQuery();
+		
+		if(rs.next()) {
+			Film film = new Film(rs.getInt("id"), rs.getString("title"), rs.getString("description"), rs.getInt("release_year"), rs.getInt("language_id"),
+							rs.getInt("rental_duration"), rs.getDouble("rental_rate"), rs.getInt("length"), rs.getDouble("replacement_cost"),
+							rs.getString("rating"), rs.getString("special_features"));
+			
+			return film;
+		} 
+
+		return null;
+	}
+
+	@Override
+	public Actor findActorById(int actorId) throws SQLException {
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		sql = "SELECT * FROM actor WHERE id = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, actorId);
+		ResultSet rs = stmt.executeQuery();
+		
+		if(rs.next()) {
+			Actor actor = new Actor(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+			
+			return actor;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) throws SQLException{
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		sql = "SELECT * FROM actor JOIN film_actor ON actor.id = film_actor.actor_id" + 
+								  "JOIN film ON film_actor.film_id = film.id" + 
+								  "WHERE film.id = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmId);
+		ResultSet rs = stmt.executeQuery();
+		
+		ArrayList<Actor> list = new ArrayList<>();
+		while(rs.next()) {
+			Actor actor = new Actor(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+			
+			list.add(actor);
+		}
+		
+		return list;
+	}
+
+}
